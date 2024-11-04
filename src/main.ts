1a7f7a2bc948d30fac056b39a451f1f06ec409b5
AntonioPereda <anapered@ucsc.edu>
2024-11-04 13:21:52 -0800

    COMMIT 0

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,24 @@
 // todo
+import "./style.css";
+
+const app: HTMLDivElement = document.querySelector("#app")!;
+
+//SETUP
+const gameName = "Test";
+document.title = gameName;
+
+const header = document.createElement("h1");
+header.innerHTML = gameName;
+app.append(header);
+
+const testButton = document.createElement("button");
+testButton.innerHTML = "TestButton";
+testButton.style.color = "green";
+testButton.style.position = "absolute";
+testButton.style.borderRadius = "100%";
+
+
+testButton.onclick = () => {
+  alert("You clicked a button!!!");
+};
+app.append(testButton);
