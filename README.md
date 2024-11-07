59ded20f8e848c6eb5600fcb832b14d7cd1f1b16
AntonioPereda <anapered@ucsc.edu>
2024-11-07 13:26:19 -0800

    D3.A DONE, READ README

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 This is the `README.md` file for the D3 project. The  changes for the current itteration are bellow:
 
-N/A, TEST BUILD
+V1
+- Deterministically generates caches that contains coins
+- Cache data, spawns and coins are random
+- User can take and deposit coins from/into your total coins
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,136 @@
-// todo
+// @deno-types="npm:@types/leaflet@^1.9.14"
+import leaflet from "leaflet";
+
+// Style sheets
+import "leaflet/dist/leaflet.css";
 import "./style.css";
 
-const app: HTMLDivElement = document.querySelector("#app")!;
+// Fix missing marker images
+import "./leafletWorkaround.ts";
 
-//SETUP
-const gameName = "Test";
-document.title = gameName;
+// Deterministic random number generator
+import luck from "./luck.ts";
 
-const header = document.createElement("h1");
-header.innerHTML = gameName;
-app.append(header);
+//random seed
+import seedrandom from 'seedrandom';
 
-const testButton = document.createElement("button");
-testButton.innerHTML = "TestButton";
-testButton.style.color = "green";
-testButton.style.position = "absolute";
-testButton.style.borderRadius = "100%";
+const seed_key = 'this is the seed value!';
+
+const random = seedrandom(seed_key);
+
+//make the map
+let zoom = 19;
+let degrees = 1e-4;
+let area_size = 6;
+let spawnrate = 0.25;
+
+const Oakes_Class = leaflet.latLng(36.98949379578401, -122.06277128548504);
+
+const map = leaflet.map('map', {
+  center: Oakes_Class,
+  zoom: zoom,
+  minZoom: zoom,
+  maxZoom: zoom,
+  zoomControl: false,
+  scrollWheelZoom: false,
+});
+
+//add details to the map
+leaflet.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: zoom,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
+
+//player marker
+const player = leaflet.marker(Oakes_Class);
+player.bindTooltip("This is you");
+player.addTo(map);
+
+//point display
+let points = 0;
+const pointPannel = document.querySelector<HTMLDivElement>("#pointPannel")!;
+pointPannel.style.fontSize = "50px";
+pointPannel.innerHTML = "0";
 
 
-testButton.onclick = () => {
-  alert("You clicked a button!!!");
-};
-app.append(testButton);
+//making the caches
+let caches: any[] = [];
+function generateCache(i: number, j: number){
+      if(luck([i,j].toString()) < spawnrate) {
+        let cache = spawnCache(i,j,caches);
+        caches.push(cache); 
+      }
+  console.log(caches);
+  return caches
+}
+
+//creates cache and details
+function spawnCache(i, j, c){
+  const origin = Oakes_Class;
+
+  let coins = Math.floor((luck([i,j].toString()) * 8 + 2));
+
+  const bounds = leaflet.latLngBounds([
+    [origin.lat + i * degrees, origin.lng + j * degrees],
+    [origin.lat + (i + 0.75) * degrees, origin.lng + (j + 0.75) * degrees],
+  ]);
+
+
+  
+  const rect = leaflet.rectangle(bounds);
+  rect.addTo(map);
+
+  //cache popup
+  let format = `
+  <div>This cache at "${i},${j}" contains <span id = "cacheCoins">${coins} </span> coins.</div>
+  <button id="take">Take</button>
+  <button id="place">Deposit</button>
+  `;
+  rect.bindPopup(()=>{
+    const popup = document.createElement("div");
+    popup.innerHTML = format;
+
+
+    //TAKE BUTTON
+    popup.querySelector<HTMLButtonElement>("#take")!.style.color = "white";
+    popup.querySelector<HTMLButtonElement>("#take")!.addEventListener("click", () => {
+      if (coins > 0){
+        coins--;
+        points++;
+        popup.querySelector<HTMLSpanElement>("#cacheCoins")!.innerHTML =
+          coins.toString();
+        pointPannel.innerHTML = `${points} coins accumulated`;
+        } else {alert("This cache is empty!");}
+    });            
+
+
+    //INSERT BUTTON
+    popup.querySelector<HTMLButtonElement>("#place")!.style.color = "white";
+    popup.querySelector<HTMLButtonElement>("#place")!.addEventListener("click", () => {
+      if (points > 0){
+        coins++;
+        points--;
+        popup.querySelector<HTMLSpanElement>("#cacheCoins")!.innerHTML =
+          coins.toString();
+        pointPannel.innerHTML = `${points} coins accumulated`;
+        } else {alert("You dont have any coins!");}
+    });   
+    return popup;
+  })
+
+  return rect
+}
+
+for (let i = -area_size; i < area_size; i++) {
+  for (let j = -area_size; j < area_size; j++) {
+    // If location i,j is lucky enough, spawn a cache!
+    if (luck([i, j].toString()) < spawnrate) {
+      generateCache(i, j);
+    }
+  }
+}
+
+
+
+
